e2fde8fcf9a0bd1b7b0a3d0c01b58c2fc2af7954
omkeshborse <bomkes25@gmail.com>
2023-07-29 10:21:12 +0530

    add style ,removing tour functionality , show more button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 const url = "https://course-api.com/react-tours-project";
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [tours, setTours] = useState(true);
+
+  const removeTour = (id) =>{
+    const newTours = tours.filter((tour) => tour.id!==id) ;
+    setTours(newTours) ;
+  }
   const fetchTours = async () => {
     setIsLoading(true);
     try {
@@ -13,13 +20,18 @@ const App = () => {
     } catch (error) {
       console.log(error);
     }
+    setIsLoading(false) ;
   };
 
   useEffect(() => {
     fetchTours();
   }, []);
 
-  return <div>App</div>;
+  if (isLoading) {
+    return <main><Loading/></main>
+  }
+
+  return <main > <Tours tours={tours} removeTour={removeTour}/></main>;
 };
 
 export default App;
